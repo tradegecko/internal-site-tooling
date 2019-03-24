b3923c2859cfc4a4acf9fd2176e973bf5f773914
billybonks <sebastienstettler@gmail.com>
2019-03-25 00:11:08 +0800

    move compiler to its own repo

---
+++ packages/@tradegecko/compiler/README.md
@@ -0,0 +1,11 @@
+# `@tradegecko/compiler`
+
+> TODO: description
+
+## Usage
+
+```
+const compiler = require('@tradegecko/compiler');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/@tradegecko/compiler/fundamentals-compiler.js
@@ -0,0 +1,36 @@
+const { join } = require('path');
+const Plugin = require('broccoli-plugin');
+
+const yaml = require('js-yaml');
+
+const {
+  writeFileSync,
+  readFileSync,
+} = require('fs');
+
+class FundementalsCompiler extends Plugin {
+  constructor(tree, options){
+    super([tree]);
+    this.options = options;
+  }
+
+  build() {
+    const inputPath = this.inputPaths[0];
+    this.options.files.forEach((options) => {
+      let dest = join(this.outputPath, options.outputFile);
+      let inputFile = join(inputPath,options.inputFile);
+      let x = readFileSync(inputFile);
+      let data = yaml.safeLoad(x.toString())
+      let dataArray = Object.keys(data).map(function(key) {
+        return {
+          ...data[key],
+          id:key
+        }
+      });
+      let serializedColours = options.serializer.serialize(dataArray);
+      writeFileSync(dest, JSON.stringify(serializedColours));
+    });
+  }
+}
+
+module.exports = FundementalsCompiler
