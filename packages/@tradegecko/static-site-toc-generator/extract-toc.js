9261dc9dfc22e9898bdde0aa51bf0d157cdb4aa5
billybonks <sebastienstettler@gmail.com>
2019-05-18 16:13:07 +0800

    initial commit for markdown-toc-generator

---
+++ packages/@tradegecko/static-site-toc-generator/extract-toc.js
@@ -0,0 +1,25 @@
+const { JSDOM } = require('jsdom');
+
+
+module.exports = function(htmlString, depth){
+  const query = buildQuery(depth)
+  const dom = new JSDOM(htmlString);
+  const nodes = dom.window.document.querySelectorAll(query);
+  const nodeArray = Array.from(nodes)
+  return nodeArray.map((node) => {
+    return {
+      level:+node.nodeName.replace('H', ''),
+      id:node.id
+    }
+  })
+}
+
+function buildQuery(depth){
+  let i = 0;
+  let nodes = []
+  while (i < depth){
+    i++;
+    nodes.push(`h${i}`);
+  }
+  return nodes.join(',')
+}
---
+++ packages/@tradegecko/static-site-toc-generator/test.js
@@ -0,0 +1,27 @@
+
+let mixed = `
+<h1 id="ourdesignprinciples">Our Design Principles</h2>
+<ol>
+<li><h2 id="asdasd">Design needs to add value, for all customers</strong></li>
+</ol>
+<p><em>Design decisions should delight all customers by always being functional, useable and reliable.</em></p>
+<ol start="2">
+<li><h2 id="qweqweqwe">Design makes the complex, simple</strong></li>
+</ol>
+<p><em>We do the heavy lifting for the user, providing trustful advice that allows them to operate fast and focus on growing their business.</em></p>
+<ol start="3">
+<li><h3 id="rererer">Data driven from empathy</strong></li>
+</ol>
+<p><em>We are driven by empathy for the user, and use data to validate that we are solving their most relevant issues first.</em></p>
+<ol start="4">
+<li><h2 id="uuuuuu">Done, tested, validated. Perfection is incremental</strong></li>
+</ol>
+<p><em>Getting small steps done and validated with users is the way to a more useful, reliable and delightful app. Testing is the fastest way to a delightful customer experience.</em></p>
+<ol start="5">
+<li><h4 id="ggggg">Design is a collaborative work</strong></li>
+</ol>
+<p><em>Getting input and quick feedback while designing gives more value to our work and allows us to iterate faster.</em></p>
+`
+extractToc = require('./extract-toc')
+let toc = extractToc(mixed, 4);
+console.log(toc)
