9e4667f90a12c4310746ea010863df638cf7b3b6
billybonks <sebastienstettler@gmail.com>
2019-05-18 16:40:14 +0800

    implement toc component

--- packages/@tradegecko/internal-sites-library/addon/components/content-navigator.js
+++ packages/@tradegecko/internal-sites-library/addon/components/content-navigator.js
@@ -1,4 +1,10 @@
 import Component from '@glimmer/component';
 
 export default class ContentNavigatorComponent extends Component {
+
+  get tocArray(){
+    let selectedTocs = this.args.toc.filter((toc) => toc.level == 3 || toc.level == 2);
+    return [this.args.toc[0], ...selectedTocs]
+
+  }
 }
