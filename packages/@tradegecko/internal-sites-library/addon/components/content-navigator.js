4b3a634fd9ef641e49f023146273d5d0da716502
billybonks <sebastienstettler@gmail.com>
2019-05-29 01:56:29 +0800

    move navigation loading into the sidebar

--- packages/@tradegecko/internal-sites-library/addon/components/content-navigator.js
+++ packages/@tradegecko/internal-sites-library/addon/components/content-navigator.js
@@ -1,9 +1,4 @@
 import Component from '@glimmer/component';
 
 export default class ContentNavigatorComponent extends Component {
-
-  get tocArray(){
-    let selectedTocs = this.args.toc.filter((toc) => toc.level == 3 || toc.level == 2);
-    return selectedTocs;
-  }
 }
